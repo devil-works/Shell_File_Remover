b3acd09fb4931a1028aac8a9908dac76b17f988d
Takahiro Ueda <tkhr6022@gmail.com>
2021-06-16 00:09:57 +0900

    Modified the number.

--- remove.sh
+++ remove.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 count=0
-while [ $count -lt 10 ]; do
-    ls -f . | head -n 10 | perl -lne unlink
+while [ $count -lt 1000 ]; do
+    ls -f . | head -n 1000 | perl -lne unlink
     count=`expr ${count} + 1`
-    sleep 2
+    sleep 1
 done
